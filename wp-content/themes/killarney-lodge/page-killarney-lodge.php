b576894e8546446416896a1f1976c4b3097e90b4
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-07-21 21:45:16 -0400

    sample image front page

--- wp-content/themes/killarney-lodge/page-killarney-lodge.php
+++ wp-content/themes/killarney-lodge/page-killarney-lodge.php
@@ -1,16 +1,19 @@
 <?php
-$collection = wp_get_page_and_children('killarney-lodge');
+$masthead = wp_get_post_by_slug('killarney-lodge', 'page');
+$blocks = wp_get_page_and_children('killarney-lodge');
 ?>
 <?php get_header() ?>
     <!-- -->
     <!-- -->
     <!-- -->
     <div class="container">
-
+        <div class="kl-masthead">
+            <?php echo $masthead->post_content; ?>
+        </div>
     </div>
     <div class="container">
         <div class="row">
-            <?php foreach ($collection as $child) { ?>
+            <?php foreach ($blocks as $child) { ?>
                 <div data-post-title="<?php echo $child->post_title; ?>"
                      class="<?php echo(get_metadata('post', $child->ID, 'className', true)); ?>">
                     <?php echo $child->post_content; ?>
