805ba1e3c8355d387d621c5efe1466483ba2ec59
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-07-29 23:47:37 -0400

    add park activities php

---
+++ wp-content/themes/killarney-lodge/page-park-activities.php
@@ -0,0 +1,25 @@
+<?php
+$masthead = wp_get_post_by_slug('park-activities', 'page');
+$blocks = wp_get_page_and_children('park-activities');
+?>
+<?php get_header() ?>
+    <!-- -->
+    <!-- -->
+    <!-- -->
+    <div class="kl-masthead">
+        <?php echo do_shortcode('[wonderplugin_slider id=1]'); ?>
+    </div>
+    <div class="container">
+        <div class="row">
+            <?php foreach ($blocks as $child) { ?>
+                <div data-post-title="<?php echo $child->post_title; ?>"
+                     class="<?php echo(get_metadata('post', $child->ID, 'className', true)); ?>">
+                    <?php echo $child->post_content; ?>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+    <!-- -->
+    <!-- -->
+    <!-- -->
+<?php get_footer() ?>
