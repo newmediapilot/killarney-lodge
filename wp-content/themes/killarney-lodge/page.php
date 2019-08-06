2d21ef34cece74404427e724697a260c17cf62b8
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-08-06 17:48:35 -0400

    transfer to page.php (reusable code)

--- wp-content/themes/killarney-lodge/page.php
+++ wp-content/themes/killarney-lodge/page.php
@@ -1,1 +1,33 @@
-page.php
+<?php
+global $post;
+$post_slug = $post->post_name;
+$master = wp_get_post_by_slug($post_slug, 'page');
+$blocks = wp_get_page_and_children($post_slug);
+?>
+<?php get_header() ?>
+    <!-- -->
+    <!-- -->
+    <!-- -->
+    <div class="kl-masthead" tabindex="-1">
+        <?php
+        $sliderId = get_metadata('post', $master->ID, 'sliderId', true);
+        if (!$sliderId) {
+            echo('<h1>no sliderId provided in post: ' . $master->ID . '</h1>');
+        }
+        echo do_shortcode('[wonderplugin_slider id=' . $sliderId . ']');
+        ?>
+    </div>
+    <div class="container">
+        <div class="row">
+            <?php foreach ($blocks as $child) { ?>
+                <div data-post-title="<?php echo $child->post_title; ?>"
+                     class="<?php echo(get_metadata('post', $child->ID, 'className', true)); ?>">
+                    <?php echo $child->post_content; ?>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+    <!-- -->
+    <!-- -->
+    <!-- -->
+<?php get_footer() ?>
