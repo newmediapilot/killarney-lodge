5cff441c1bb5bbe002fae830e3eeffeb4b7dbff0
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-08-18 23:01:22 -0400

    should be empty

--- wp-content/themes/killarney-lodge/single.php
+++ wp-content/themes/killarney-lodge/single.php
@@ -1,8 +1,1 @@
-<?php
-if(has_term('movies', 'category', $post)) {
-    get_template_part('single-movies', 'movies');
-} else {
-    // use default template file single-template.php
-    get_template_part('single-template');
-}
-?>
+single.php
