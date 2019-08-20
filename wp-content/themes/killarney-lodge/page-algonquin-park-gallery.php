00f82b8337f4ca36310044c5b1a8233468658ae1
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-08-20 00:59:11 -0400

    add gallery template and containers

---
+++ wp-content/themes/killarney-lodge/page-algonquin-park-gallery.php
@@ -0,0 +1,30 @@
+<?php get_header() ?>
+    <!-- begin algonquin-park-gallery.php -->
+    <!-- begin algonquin-park-gallery.php -->
+    <!-- begin algonquin-park-gallery.php -->
+    <div class="gallery-images container">
+        <div class="row">
+            <div class="col-12">
+                <?php echo do_shortcode('[wonderplugin_slider id=16]'); ?>
+            </div>
+        </div>
+    </div>
+    <div class="gallery-videos container pb-5">
+        <div class="row">
+            <div class="col-12">
+                <?php echo do_shortcode('[wonderplugin_slider id=17]'); ?>
+            </div>
+        </div>
+    </div>
+    <div class="gallery-tours container pb-5 mb-5">
+        <div class="row">
+            <div class="col-12">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sin!4v1490184751929!6m8!1m7!1sOXdaj-uFdc0AAAQ1mHGKNg!2m2!1d45.58379699848677!2d-78.48600600429097!3f238!4f0!5f0.7820865974627469"
+                        width="100%" height="365" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+            </div>
+        </div>
+    </div>
+    <!-- end algonquin-park-gallery.php -->
+    <!-- end algonquin-park-gallery.php -->
+    <!-- end algonquin-park-gallery.php -->
+<?php get_footer() ?>
