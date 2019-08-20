abcd1f9812caecb7362ed88d988750c8f339de8e
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-08-20 01:19:13 -0400

    update colors and padding

--- wp-content/themes/killarney-lodge/page-algonquin-park-gallery.php
+++ wp-content/themes/killarney-lodge/page-algonquin-park-gallery.php
@@ -14,14 +14,14 @@
         <!-- relax enjoy explore -->
         <!-- relax enjoy explore -->
     </div>
-    <div class="gallery-videos container pb-5">
+    <div class="gallery-videos container">
         <div class="row">
             <div class="col-12">
                 <?php echo do_shortcode('[wonderplugin_slider id=17]'); ?>
             </div>
         </div>
     </div>
-    <div class="gallery-tours container pb-5 mb-5">
+    <div class="gallery-tours container">
         <div class="row">
             <div class="col-12">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sin!4v1490184751929!6m8!1m7!1sOXdaj-uFdc0AAAQ1mHGKNg!2m2!1d45.58379699848677!2d-78.48600600429097!3f238!4f0!5f0.7820865974627469"
