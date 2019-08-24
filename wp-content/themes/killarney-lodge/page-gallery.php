5ae49f13ac2a92da119e1bc69cf418b9b6c91b65
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-08-24 15:08:47 -0400

    rework gallery

---
+++ wp-content/themes/killarney-lodge/page-gallery.php
@@ -0,0 +1,25 @@
+<?php get_header() ?>
+    <!-- begin algonquin-park-gallery.php -->
+    <!-- begin algonquin-park-gallery.php -->
+    <!-- begin algonquin-park-gallery.php -->
+    <div class="gallery-images">
+        <div class="gallery-slider">
+            <?php echo do_shortcode('[wonderplugin_slider id=16]'); ?>
+        </div>
+    </div>
+    <div class="gallery-divider">
+        <img title="Relax Explore Enjoy" src="/wp-content/themes/killarney-lodge/res/relaxExploreEnjoy.png">
+    </div>
+    <div class="gallery-videos">
+        <div class="gallery-slider">
+            <?php echo do_shortcode('[wonderplugin_slider id=17]'); ?>
+        </div>
+    </div>
+    <div class="gallery-tours">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sin!4v1490184751929!6m8!1m7!1sOXdaj-uFdc0AAAQ1mHGKNg!2m2!1d45.58379699848677!2d-78.48600600429097!3f238!4f0!5f0.7820865974627469"
+                width="400" height="400" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+    </div>
+    <!-- end algonquin-park-gallery.php -->
+    <!-- end algonquin-park-gallery.php -->
+    <!-- end algonquin-park-gallery.php -->
+<?php get_footer() ?>
