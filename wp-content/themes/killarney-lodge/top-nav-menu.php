57e02a27183388e05a0c48671d2b70b96db3d41b
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-08-25 00:30:27 -0400

    building top nav bootstrap

--- wp-content/themes/killarney-lodge/top-nav-menu.php
+++ wp-content/themes/killarney-lodge/top-nav-menu.php
@@ -1,22 +1,27 @@
 <!-- begin top-nav-menu -->
 
 <?php $home_menu = wp_get_nav_menu_items('Home Menu'); ?>
-<?php $main_menu = wp_get_nav_menu_items('Main Menu'); ?>
+<?php $main_menu = wp_get_nav_menu_items('Default Top Menu'); ?>
 <?php $phone_menu = wp_get_nav_menu_items('Phone Menu'); ?>
 <?php $reservation_menu = wp_get_nav_menu_items('Reservation Menu'); ?>
 
-<nav class="kl-navbar--mobile">
-    <div class="kl-navbar--mobile--wrapper">
-        <div class="kl-navbar--mobile--trigger">
-
-        </div>
-    </div>
-</nav>
+<?php
+$main_menu_parents = array_filter($main_menu, function ($footer_item) {
+    return $footer_item->menu_item_parent == "0";
+});
+foreach ($main_menu_parents as $menu_item_parent) {
+    $menu_item_parent->menu_item_children = array_filter($main_menu, function ($footer_item) use ($menu_item_parent) {
+        return $footer_item->menu_item_parent == $menu_item_parent->ID;
+    });
+}
+//var_dump($main_menu_parents);
+?>
 
 <nav class="kl-navbar--desktop">
     <!-- top-nav-desktop -->
     <div class="kl-navbar-wrapper">
         <div class="kl-navbar--holder">
+
             <div class="kl-navbar--menu">
                 <a href="<?php echo $home_menu[0]->url ?>"
                    title="<?php echo $home_menu[0]->title ?>">
@@ -24,15 +29,40 @@
                          alt="<?php echo $home_menu[0]->title ?>">
                 </a>
             </div>
+
             <div class="kl-navbar--items">
-                <ul>
-                    <?php foreach ($main_menu as $menu_item) { ?>
-                        <li>
-                            <a href="<?php echo $menu_item->url ?>"
-                               title="<?php echo $menu_item->title ?>"><?php echo $menu_item->title ?></a>
-                        </li>
-                    <?php } ?>
-                </ul>
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <?php foreach ($main_menu_parents as $main_menu_parent) { ?>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="<?php echo $main_menu_parent->url ?>"
+                                       id="navbarDropdown<?php echo $main_menu_parent->id ?>" role="button"
+                                       title="<?php echo $main_menu_parent->title ?>"
+                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <?php echo $main_menu_parent->title ?>
+                                    </a>
+                                    <div class="dropdown-menu"
+                                         aria-labelledby="navbarDropdown<?php echo $main_menu_parent->id ?>">
+                                        <?php foreach ($main_menu_parent->menu_item_children as $menu_child) { ?>
+                                            <a class="dropdown-item" title="<?php echo $menu_child->title ?>"
+                                               href="<?php echo $menu_child->url ?>"><?php echo $menu_child->title ?></a>
+                                        <?php } ?>
+                                    </div>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                </nav>
+
             </div>
             <div class="kl-navbar--phone">
                 <a href="<?php echo $phone_menu[0]->url ?>"
