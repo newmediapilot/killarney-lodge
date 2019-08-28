51a17a31452e21fa77b1bd6393677b31d520b268
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-08-28 16:47:22 -0400

    reqrote cabin rates mechanism

--- wp-content/themes/killarney-lodge/page-rates.php
+++ wp-content/themes/killarney-lodge/page-rates.php
@@ -7,11 +7,28 @@ $blocks = wp_get_page_and_children('rates');
     <!-- -->
     <div class="kl-masthead" tabindex="-1">
         <div class="kl-cabins">
-            <div class="kl-cabins--image">
-                <!-- populated by JS -->
+            <div class="kl-cabins--images">
+                <img class="kl-cabins--image--show"
+                     src="/wp-content/themes/killarney-lodge/res/cabin-map/1-Shoreline.jpg"/>
+                <img src="/wp-content/themes/killarney-lodge/res/cabin-map/2-MainLake.jpg"/>
+                <img src="/wp-content/themes/killarney-lodge/res/cabin-map/3-PrimeLake.jpg"/>
+                <img src="/wp-content/themes/killarney-lodge/res/cabin-map/4-Duplex.jpg"/>
+                <img src="/wp-content/themes/killarney-lodge/res/cabin-map/5-TwoBedroom.jpg"/>
+                <img src="/wp-content/themes/killarney-lodge/res/cabin-map/6-Crowe.jpg"/>
             </div>
             <div class="kl-cabins--selectors">
-                <!-- populated by JS -->
+                <img onmouseover="document.setCabinImage(0)" onmousedown="document.setCabinImage(0)"
+                     src="/wp-content/themes/killarney-lodge/res/cabin-map/image1-shoreline.jpg"/>
+                <img onmouseover="document.setCabinImage(1)" onmousedown="document.setCabinImage(1)"
+                     src="/wp-content/themes/killarney-lodge/res/cabin-map/image2-mainlake.jpg"/>
+                <img onmouseover="document.setCabinImage(2)" onmousedown="document.setCabinImage(2)"
+                     src="/wp-content/themes/killarney-lodge/res/cabin-map/image3-prime-lake.jpg"/>
+                <img onmouseover="document.setCabinImage(3)" onmousedown="document.setCabinImage(3)"
+                     src="/wp-content/themes/killarney-lodge/res/cabin-map/image4-duplex.jpg"/>
+                <img onmouseover="document.setCabinImage(4)" onmousedown="document.setCabinImage(4)"
+                     src="/wp-content/themes/killarney-lodge/res/cabin-map/image5-two-bedroom.jpg"/>
+                <img onmouseover="document.setCabinImage(5)" onmousedown="document.setCabinImage(5)"
+                     src="/wp-content/themes/killarney-lodge/res/cabin-map/image6-crowe-cottage.jpg"/>
             </div>
         </div>
     </div>
--- wp-content/themes/killarney-lodge/src/js/main.js
+++ wp-content/themes/killarney-lodge/src/js/main.js
@@ -6,66 +6,11 @@
  * cabins map
  */
 (function ($) {
-    /**
-     * sources
-     */
-    var root = "/wp-content/themes/killarney-lodge/res/cabin-map/";
-    var images = [
-        {panel: "1-Shoreline", button: "image1-shoreline"},
-        {panel: "2-MainLake", button: "image2-mainlake"},
-        {panel: "3-PrimeLake", button: "image3-prime-lake"},
-        {panel: "4-Duplex", button: "image4-duplex"},
-        {panel: "5-TwoBedroom", button: "image5-two-bedroom"},
-        {panel: "6-Crowe", button: "image6-crowe-cottage"}
-    ];
-    /**
-     * setup master image
-     */
-    var holder = $('.kl-cabins--image');
-    var map = $('<img>');
-
-    /**
-     * click action
-     */
-    function setImage(index) {
-        var image = images[index];
-        map.attr(
-            'src',
-            [
-                root,
-                image.panel,
-                '.jpg'
-            ].join('')
-        );
+    document.setCabinImage = function (index) {
+        var images = $('.kl-cabins--images img');
+        $(images).removeClass('kl-cabins--image--show');
+        $(images[index]).addClass('kl-cabins--image--show');
     }
-
-    /**
-     * create selectors
-     */
-    for (var i = 0; i < images.length; i++) {
-        var container = $('.kl-cabins--selectors');
-        var source = [
-            root,
-            images[i].button,
-            '.jpg'
-        ].join('');
-        var image = $('<img>');
-        image.attr('src', source);
-        (function (index) {
-            image.bind('click mouseover', (function () {
-                setImage(index)
-            }));
-        })(i);
-        var preload = new Image();
-        preload.src = source;
-        container.append(image);
-    }
-    /**
-     * setup holder
-     */
-    setImage(0);
-    holder.append(map);
-
 })(jQuery);
 
 /**
