d29734928a016b5e44f37d02d99d08a4e11a7998
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-10-17 02:13:33 -0400

    add google tag manager

--- wp-content/themes/killarney-lodge/header.php
+++ wp-content/themes/killarney-lodge/header.php
@@ -8,6 +8,15 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <?php include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); ?>
     <?PHP wp_head(); ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23837992-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-23837992-1');
+    </script>
 </head>
 <body <?php body_class(); ?>>
 
