6e7b3282c39d9f12d2ab29e46f1a2cdc6d700e1d
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-10-24 15:36:21 -0400

    add a space

--- wp-content/themes/killarney-lodge/footer.php
+++ wp-content/themes/killarney-lodge/footer.php
@@ -9,7 +9,7 @@
 <div class="footer-credits">
     © 2019, Killarney Lodge, Algonquin Park, Ontario. All Rights Reserved.<br>
     <a href="https://www.christopher-dew.com" target="_blank">
-        <span>Design + Principle Photography</span><span>Christopher Dew</span></a>
+        <span>Design + Principle Photography</span>&nbsp;<span>Christopher Dew</span></a>
     <br>
     <a href="https://www.linkedin.com/in/newmediapilot" target="_blank">Website Development by Marcin Zajkowski</a><br>
 </div>
