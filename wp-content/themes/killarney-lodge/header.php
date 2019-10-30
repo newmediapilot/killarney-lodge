a494e82faf4204a215c80503d196ce3ef33e20a4
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-10-30 17:06:12 -0400

    add description override

--- wp-content/themes/killarney-lodge/header.php
+++ wp-content/themes/killarney-lodge/header.php
@@ -7,11 +7,14 @@
     <?php
         global $post;
         $master = $post;
-        $seo_title_source = get_metadata('post', $master->ID, 'title', true)
+        $seo_title_source = get_metadata('post', $master->ID, 'title', true);
+        $seo_description_source = get_metadata('post', $master->ID, 'description', true);
     ?>
     <title><?php bloginfo('name'); ?><?php wp_title(); ?><?php if($seo_title_source){ echo ' &raquo; '.$seo_title_source; } ?></title>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <?php include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); ?>
+    <?php if($seo_title_source){ echo ' &raquo; '.$seo_title_source; } ?>
+    <?php if($seo_description_source){ echo '<meta name="description" content="'.$seo_description_source.'" />'; ?>
     <?PHP wp_head(); ?>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23837992-1"></script>
@@ -19,7 +22,6 @@
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-
         gtag('config', 'UA-23837992-1');
     </script>
 </head>
