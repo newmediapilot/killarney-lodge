078460322aae1077ffb0391379c62e82150f46ca
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2020-05-06 17:44:20 -0400

    working on vr tour and gallery standalone

---
+++ wp-content/themes/killarney-lodge/page-gallery-virtual-tour.php
@@ -0,0 +1,14 @@
+<?php get_header() ?>
+    <div class="gallery-tour">
+        <div class="gallery-slogan">
+            <img title="Relax Explore Enjoy"
+                 aria-hidden="true"
+                 src="/wp-content/themes/killarney-lodge/res/relaxExploreEnjoy.png">
+        </div><!-- /end gallery-slogan -->
+        <div class="virtual-tour">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sin!4v1490184751929!6m8!1m7!1sOXdaj-uFdc0AAAQ1mHGKNg!2m2!1d45.58379699848677!2d-78.48600600429097!3f238!4f0!5f0.7820865974627469"
+                    width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+                    tabindex="0"></iframe>
+        </div>
+    </div>
+<?php get_footer() ?>
--- wp-content/themes/killarney-lodge/page-gallery.php
+++ wp-content/themes/killarney-lodge/page-gallery.php
@@ -1,13 +1,12 @@
 <?php get_header() ?>
-    <!-- begin algonquin-park-gallery.php -->
-    <!-- begin algonquin-park-gallery.php -->
-    <!-- begin algonquin-park-gallery.php -->
     <div class="gallery-images">
+        <div class="gallery-slogan">
+            <img title="Relax Explore Enjoy"
+                 aria-hidden="true"
+                 src="/wp-content/themes/killarney-lodge/res/relaxExploreEnjoy.png">
+        </div><!-- /end gallery-slogan -->
         <div class="gallery-slider">
             <?php echo do_shortcode('[wonderplugin_slider id=16]'); ?>
         </div>
     </div>
-    <!-- end algonquin-park-gallery.php -->
-    <!-- end algonquin-park-gallery.php -->
-    <!-- end algonquin-park-gallery.php -->
 <?php get_footer() ?>
