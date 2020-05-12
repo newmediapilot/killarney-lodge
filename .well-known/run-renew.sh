4042df6c73dcade106b0fd63915e94d4d1987ba7
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2020-05-11 21:34:05 -0400

    add more echoe

--- .well-known/run-renew.sh
+++ .well-known/run-renew.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
+echo 'STARTING!'
 DOMAIN=killarneylodge.com
 echo $DOMAIN
+echo 'starting certbot complete...'
 sudo certbot -n -d www.${DOMAIN} -d ${DOMAIN} --webroot -w /home/bitnami/apps/wordpress/htdocs/ --preferred-challenges http certonly
 echo 'running certbot complete...'
+echo 'backup old certs...'
 sudo mv /opt/bitnami/apache2/conf/server.key /opt/bitnami/apache2/conf/server.key.old
 sudo mv /opt/bitnami/apache2/conf/server.crt /opt/bitnami/apache2/conf/server.crt.old
-echo 'moving old cert file complete...'
+echo 'backup old certs complete...'
+echo 'linking new cert files...'
 sudo ln -s /etc/letsencrypt/live/www.${DOMAIN}/privkey.pem /opt/bitnami/apache2/conf/server.key
 sudo ln -s /etc/letsencrypt/live/www.${DOMAIN}/fullchain.pem /opt/bitnami/apache2/conf/server.crt
-echo 'linking new cert file complete...'
+echo 'linking new cert files complete...'
+echo 'restart...'
 sudo /opt/bitnami/ctlscript.sh restart
-echo 'restart complete... ALL DONE!'
+echo 'restart complete'
+echo 'ALL DONE!'
