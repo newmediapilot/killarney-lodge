e0faeca539ed72017c8120d24dea5c954bf1fce6
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2020-05-11 21:29:19 -0400

    make scrip non-interactive

--- .well-known/run-renew.sh
+++ .well-known/run-renew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DOMAIN=killarneylodge.com
 echo $DOMAIN
-sudo certbot -d www.${DOMAIN} -d ${DOMAIN} --webroot -w /home/bitnami/apps/wordpress/htdocs/ --preferred-challenges http certonly
+sudo certbot -n -d www.${DOMAIN} -d ${DOMAIN} --webroot -w /home/bitnami/apps/wordpress/htdocs/ --preferred-challenges http certonly
 echo 'running certbot complete...'
 sudo mv /opt/bitnami/apache2/conf/server.key /opt/bitnami/apache2/conf/server.key.old
 sudo mv /opt/bitnami/apache2/conf/server.crt /opt/bitnami/apache2/conf/server.crt.old
