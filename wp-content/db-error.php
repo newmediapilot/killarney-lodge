b17ec6ddb6506c074148b6c2be82750c77a3772e
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2021-06-01 13:07:15 -0400

    add db error handler page

---
+++ wp-content/db-error.php
@@ -0,0 +1,17 @@
+<?php
+
+header('HTTP/1.1 503 Service Temporarily Unavailable');
+header('Status: 503 Service Temporarily Unavailable');
+
+?>
+<!DOCTYPE HTML>
+<html dir="ltr" lang="en-US">
+<head>
+    <title>503 Service Temporarily Unavailable</title>
+</head>
+<body style="font-family:sans-serif">
+<img width="320" src="https://www.killarneylodge.com/wp-content/themes/killarney-lodge/res/LOGO.png">
+<h1>There's a temporary issue, we're fixing it!</h1>
+<p>While we do, just call <a href="tel:1-866-473-5551">1-866-473-5551</a> for reservations and information.</p>
+</body>
+</html>
